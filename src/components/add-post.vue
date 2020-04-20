4ce7ea9e8212480c8a6e5e3b3d8a18d3695c1185
Shumas <shumaslaghari@gmail.com>
2020-04-20 19:10:12 +0500

    form submission completed

--- src/components/add-post.vue
+++ src/components/add-post.vue
@@ -5,13 +5,19 @@
       <div class="row">
         <div class="col-8 offset-2">
           <b-form @submit.prevent="onSubmit">
-            <b-form-group id="parent" label="Select Parent Post(Optional)" label-for="parent" class="text-info">
+            <!-- <b-form-group id="parent" label="Select Parent Post (Optional)" label-for="parent" class="text-info">
               <b-form-select
                 id="parent"
-                v-model="form.food"
+                v-model="form.parent"
                 :options="parents"
+                 @change="test($event)"
               ></b-form-select>
-            </b-form-group>
+            </b-form-group> -->
+            <select name="" id="" @change="test($event)" class="w-100 d-block" >
+              <option v-for="(parent,index) in parents" :value="parent.text" :dataId="parent.id" :key="index">{{parent.text}}
+              </option>
+              
+            </select>
 
             <b-form-group
               id="title"
@@ -42,7 +48,8 @@
               </b-form-textarea>             
             </b-form-group>
 
-            <b-form-group id="tags" label="Post Tags:" label-for="tags" class="text-info">
+            <b-form-group id="tags" label="Post Tags:" label-for="tags" class="text-info" 
+            description="Seperate tags by commas">
               <b-form-input
                 id="tags"
                 v-model="form.tags"
@@ -56,24 +63,40 @@
         </div>
       </div>
     </div>
-
   </div>
+
 </template>
 
 <script>
-  import axios from "axios"
+  import axios from 'axios'
+  import {mapActions,mapGetters} from 'vuex'
   export default {
+    created(){
+      axios.get('/getposts').then(posts=>{
+        console.log(posts)
+        this.$store.dispatch('fetchPosts',posts.data)
+        posts.data.forEach(post=>{
+          this.parents.push({text:post.title,value:post.title,id:post.id})
+        })
+      }).catch(err=>{
+        console.log(err)
+      })
+    },
     data() {
       return {
         form: {
           title: '',
-          name: '',
           content:'',
           parent: null,
-          checked: []
+          id:null
         },
-        parents: [{ text: 'Select One', value: null }, 'Carrots', 'Beans', 'Tomatoes', 'Corn'],
+        parents: [{ text: 'Select One', value: null }],
       }
+    },
+    computed:{
+      ...mapGetters([
+        "getPosts"
+		  ])
     },
     methods: {
       onSubmit(evt) {
@@ -84,6 +107,10 @@
           console.log(err)
         })
       },
+      test(event){
+          this.form.id=event.target.options[event.target.options.selectedIndex].getAttribute('dataId')
+          console.log(this.form.id)
+      }
     }
   }
 </script>
