5f4d55e0e768cf02ef2a69a3bbb6efd6aca480f6
Shumas <shumaslaghari@gmail.com>
2020-04-20 19:07:03 +0500

    completed form submission

--- api/routes.js
+++ api/routes.js
@@ -6,18 +6,25 @@ const Router = express.Router();
 //Posts Array
 let posts=[]
 
-// Router.get('/',(req,res,next)=>{
-//   res.send({
-//     name:'shumas'
-//   })
-// })
+Router.get('/getposts',(req,res,next)=>{
+  fs.readFile('posts.json',(err,data)=>{
+    if(err){
+      return res.status(404).send({message:'Posts not found'})
+    }
+    posts=JSON.parse(data);
+    res.send(posts)
+  })
+})
 
 Router.post('/createpost',(req,res,next)=>{
   fs.readFile('posts.json',(err,data)=>{
     if(!err){
       posts=JSON.parse(data);
     }
-    req.body.id=uuidv4()
+    if(!req.body.id){
+      req.body.id=uuidv4()
+    }
+    
     posts.push(req.body)
     fs.writeFile('posts.json',JSON.stringify(posts),(err)=>{
     res.send({message:'Post Added'})
